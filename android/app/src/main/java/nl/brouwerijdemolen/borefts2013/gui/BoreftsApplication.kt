1ab317f559014fc8d33dd429e67a027b3d80be76
Eric Kok <erickok@gmail.com>
2018-05-21 20:22:40 +0100

    Remove DI (Koin) logger.

--- android/app/src/main/java/nl/brouwerijdemolen/borefts2013/gui/BoreftsApplication.kt
+++ android/app/src/main/java/nl/brouwerijdemolen/borefts2013/gui/BoreftsApplication.kt
@@ -2,12 +2,13 @@ package nl.brouwerijdemolen.borefts2013.gui
 
 import android.app.Application
 import org.koin.android.ext.android.startKoin
+import org.koin.log.EmptyLogger
 
 class BoreftsApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        startKoin(this, listOf(networkModule, uiModule, viewModelsModule))
+        startKoin(this, listOf(networkModule, uiModule, viewModelsModule), logger = EmptyLogger())
     }
 
 }
