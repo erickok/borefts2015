7fe52b0e36ec2fc4d0ab61bcc8a3538ab311952b
Eric Kok <erickok@gmail.com>
2018-08-07 21:11:26 +0200

    Update dependencies

--- android/app/src/main/java/nl/brouwerijdemolen/borefts2013/gui/components/LogExtensions.kt
+++ android/app/src/main/java/nl/brouwerijdemolen/borefts2013/gui/components/LogExtensions.kt
@@ -3,4 +3,4 @@ package nl.brouwerijdemolen.borefts2013.gui.components
 import android.util.Log
 import arrow.core.Failure
 
-fun Failure<*>.log() = Log.w("Try", this.exception.toString())
+fun Failure.log() = Log.w("Try", this.exception.toString())
